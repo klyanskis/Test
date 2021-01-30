3c70d94ee7d87124002015cd4b75efb8f5eac081
klyan <klyan@LAPTOP-K89BALBG>
2021-01-30 17:09:14 +0700

    add prius name

--- CarManager/src/ua/com/foxminded/carmanager/CarManager.java
+++ CarManager/src/ua/com/foxminded/carmanager/CarManager.java
@@ -7,7 +7,7 @@ public class CarManager {
 
 	public static void main(String[] args) {
 
-		Car prius = new PassengerCar("Toyota Prius1", 2008, 15000, 1200, Color.GREEN);
+		Car prius = new PassengerCar("Toyota Prius123", 2008, 15000, 1200, Color.GREEN);
 		Car renault = new Bus("Renault Laguna", 2000, 9700, 1600, Color.WHITE);
 		Car renault2 = new Bus("Renault Laguna", 2000, 9700, 1600, Color.WHITE);
 		Car Fura = new Truck("Fura", 2020, 1000000, 5000, Color.RED);
